7772df043253c4f3777170d010ca5c70a2d7793a
Ayfred <maxime.mu@mines-ales.org>
2021-11-30 11:12:42 +0100

    ajout du plateau image

--- src/monopoly/Plateau.java
+++ src/monopoly/Plateau.java
@@ -31,7 +31,7 @@ public class Plateau extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\belkh\\Documents\\GitHub\\Monopoly\\plateau.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\maxim\\Downloads\\monopoly-classique-plateau.jpg")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
